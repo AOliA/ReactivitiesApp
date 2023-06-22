0e44b89c7ecf522fc37a21b75df3265a5c74ed39
André O. Almeida <andrewdrew647@gmail.com>
2023-06-22 23:27:01 +0000

    Prepare for app build

--- client-app/src/app/stores/commentStore.ts
+++ client-app/src/app/stores/commentStore.ts
@@ -1,4 +1,4 @@
-import {
+ import {
   HubConnection,
   HubConnectionBuilder,
   LogLevel,
@@ -18,7 +18,7 @@ export default class CommentStore {
   createHubConnection = (activityId: string) => {
     if (store.activityStore.selectedActivity) {
       this.hubConnection = new HubConnectionBuilder()
-        .withUrl("http://localhost:5000/chat?activityId=" + activityId, {
+        .withUrl(process.env.REACT_APP_CHAT_URL + "?activityId=" + activityId, {
           accessTokenFactory: () => store.userStore.user?.token!,
         })
         .withAutomaticReconnect()
